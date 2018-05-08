cfaa9b30ebdeb755e4ae3d1fb862ac165fde55cc
Haruka Matsumoto <harukam0416@gmail.com>
2018-05-08 19:19:33 +0900

    Create a script to run tests

---
+++ test_run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+src_dir="src"
+test_dir="test"
+out_dir=$test_dir
+
+for fname in `ls ${test_dir}/*.txt | xargs -n 1 basename`
+do
+  input="${test_dir}/${fname}"
+  output="${out_dir}/${fname}.html"
+  exe="python ${src_dir}/conv_main.py ${input} ${output}"
+  echo $exe
+  ${exe}
+done
