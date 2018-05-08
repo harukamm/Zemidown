c87f92f9d830a6af3d29032d369a4e626d407c45
Haruka Matsumoto <harukam0416@gmail.com>
2018-05-08 18:50:48 +0900

    Initial version of Zemidown

---
+++ src/conv_main.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+from domtree import *
+import re
+import sys
+
+title = "tapl ch 1 - 3"
+keys = ["ch", "name", "sec", "file"]
+ch_lst = []
+default_style = [("tr.inf_down",
+                  [("border-top", "1px solid #000")]),
+                 ("tr.inf_down > td",
+                  [("text-align", "center"),
+                   ("padding", "0 10px")]),
+                 ("tr.inf_up > td",
+                  [("padding", "0px 10px"),
+                   ("text-align", "center")]),
+                 ("td.inf_name",
+                  [("vertical-align", "middle"),
+                   ("font-size", "50%")]),
+                 ("table.inf",
+                  [("border-collapse", "collapse"),
+                   ("padding", "10px")]),
+                 ("tr.horizon > td",
+                  [("padding", "0 10px")]),
+                 ("ul.no_num",
+                  [("list-style", "none")]),
+                 (".box",
+                  [("padding", "10px"),
+                   ("margin-bottom", "10px"),
+                   ("display", "inline-block"),
+                   ("border", "1px solid #333")]),
+                 (".ch_title",
+                  [("font-weight", "bolder"),
+                   ("font-size", "130%")]),
+                 (".ch",
+                  [("margin", "10px 0")]),
+                 (".ch_content",
+                  []),
+                 (".sec_title",
+                  [("font-weight", "normal"),
+                   ("font-size", "110%")]),
+                 (".sec",
+                  [("margin", "20px 0")]),
+                 (".sec_content",
+                  [("margin-left", "20px")])]
+styles = None
+
+doctype_tag = "<!DOCTYPE html>"
+coding = "UTF-8"
+meta_tag = "<meta charset=\"" + coding + "\">"
+body_elem = Element("body")
+
+def init_styles():
+    global styles
+    global default_style
+    styles = Styles()
+    for secretor , props in default_style:
+        b = StyleBlock(secretor)
+        for p , v in props:
+            b.appendProp(p, v)
+        styles.appendBlock(b)
+    # print styles.to_css()
+
+def parse_file(fname):
+    f = open(fname, "r")
+    lines = f.readlines()
+    f.close()
+    i = 0
+    length = len(lines)
+
+    # read header
+    header = []
+    while True:
+        if i == length:
+            raise Exception("header error")
+        elif lines[i][0] == '#':
+            break
+        elif lines[i] != "":
+            header.append(lines[i])
+        i += 1
+
+    # read chapters
+    chapters = []
+    while True:
+        if length <= i:
+            break
+        line = lines[i]
+        m = re.match(r':ch +(\d+)$', line)
+        if m:
+            ch_n = int (m.group(1))
+            ptr, chapter = parse_chapter(ch_n, i + 1, lines)
+            i = ptr
+            chapters.append(chapter)
+            continue
+        i += 1
+    return chapters
+
+def parse_content_until_end(ptr, lines):
+    i = ptr
+    content = []
+    while True:
+        if len(lines) <= i:
+            raise Exception("not found :e command")
+
+        line = lines[i]
+        if line.rstrip() == ":e":
+            i += 1
+            break
+        else:
+            content.append(line)
+        i += 1
+    return (i, content)
+
+# section with no content is invalid
+# section can handle only :s command
+def parse_section(ch_n, sec_n, ptr, lines):
+    title = lines[ptr]
+    i = ptr
+
+    sec_content = []
+    while True:
+        if len(lines) <= i:
+            break
+        
+        line = lines[i]
+        if line.rstrip() == ":s":
+            ptr , content = parse_content_until_end(i + 1, lines)
+            i = ptr
+            sec_content = content
+            break
+        elif line[0] == ':':
+            raise Exception("Unexpected command")
+        i += 1
+    return i , Section(ch_n, sec_n, title, sec_content)
+
+# chapter can be empty
+# chapter reads lines until next :ch command or end of lines
+def parse_chapter(ch_n, ptr, lines):
+    title = lines[ptr]
+    i = ptr + 1
+
+    # read chapter_content if it exists
+    chapter_content = []
+    while True:
+        if len(lines) <= i:
+            return i , Chapter(ch_n, title, [], [])
+
+        line = lines[i]
+        if line.startswith(":sec"):
+            break
+        elif line.rstrip() == (":s"):
+            ptr , content = parse_content_until_end(i + 1, lines)
+            i = ptr
+            chapter_content = content
+            break
+        elif line.startswith(":ch"):
+            return i , Chapter(ch_n, title, [], [])
+        i += 1
+
+    # read sections
+    sections = []
+    while True:
+        if len(lines) <= i:
+            break
+
+        line = lines[i]
+        m = re.match(r':sec +(\d+)$', line)
+        if m:
+            sec_n = int (m.group(1))
+            ptr , section = parse_section(ch_n, sec_n, i + 1, lines)
+            i = ptr
+            sections.append(section)
+        elif line.startswith(":ch"):
+            break
+        i += 1
+
+    return i , Chapter(ch_n, title, chapter_content, sections)
+
+def create_chapters_html(chapters):
+    root = Element("ul.no_num")
+    for ch in chapters:
+        li = Element("li")
+        li.appendChild(ch.to_html())
+        root.appendChild(li)
+    return root
+
+def create_index(chapters):
+    ch_ul = Element("ul")
+    for ch in chapters:
+        ch_li = Element("li")
+        ch_link = LinkNode("#ch" + str(ch.n), "ch." + str(ch.n))
+        ch_li.appendChild(ch_link)
+        ch_li.appendChild(TextNode(" " + ch.title))
+        sc_ul = Element("ul.no_num")
+        for sec in ch.sections:
+            li = Element("li")
+            sc_id = "#sec" + str(ch.n) + "_" + str(sec.n)
+            sc_link = LinkNode(sc_id, str(ch.n) + "-" + str(sec.n))
+            li.appendChild(sc_link)
+            li.appendChild(TextNode(" " + sec.title))
+            sc_ul.appendChild(li)
+        ch_li.appendChild(sc_ul)
+        ch_ul.appendChild(ch_li)
+    return ch_ul
+
+def body_to_html():
+    return body_elem.to_html()
+
+def generate_html(output_fname):
+    f = open(output_fname, 'w')
+    f.write(doctype_tag)
+    f.write("<html>\n")
+    f.write("<head>\n")
+    f.write("<style>\n")
+    f.write(styles.to_css())
+    f.write("</style>\n")
+    f.write(meta_tag)
+    f.write("\n")
+    f.write("<title>" + title + "</title>")
+    f.write("</head>")
+    f.write(body_to_html())
+    f.write("</html>\n")
+    f.close()
+    return
+
+def on_load():
+    args = sys.argv
+    if len(args) != 3 and len(args) != 4:
+        print "syntax: python conv_main.py <file-path> <output> [title]"
+        return
+    init_styles()
+    input_fname = args[1];
+    output_fname = args[2];
+    if len(args) == 4:
+        global title
+        title = args[3]
+    chapters = parse_file(input_fname)
+    body_elem.appendChild(create_index(chapters))
+    body_elem.appendChild(create_chapters_html(chapters))
+    generate_html(output_fname)
+
+on_load()
---
+++ src/domtree.py
@@ -0,0 +1,468 @@
+import re
+
+env = []
+########################################
+# Class for temporary DOM element
+########################################
+class Element:
+    def __init__(self, n):
+        self.children = []
+        self.tag = None
+        self.ids = []
+        self.classes = []
+        self.attributes = []
+        for m in re.finditer(r'([#.])(\w+)', n):
+            if self.tag is None:
+                self.tag = n[:m.start()]
+            if m.group(1) == '#':
+                self.ids.append(m.group(2))
+            elif m.group(1) == '.':
+                self.classes.append(m.group(2))
+        if self.tag is None:
+            self.tag = n
+
+    def appendChild(self, elem):
+        if elem.to_html is None:
+            raise Exception("appendChild")
+        self.children.append(elem)
+        return self
+
+    def setAttribute(self, p, v):
+        self.attributes.append((p, v))
+
+    def br(self):
+        self.appendChild(Br())
+        return self
+
+    def addClass(self, c):
+        self.classes.append(c)
+        return self
+    def to_html(self):
+        s = "<" + self.tag
+        if len(self.ids) > 0 :
+            s += " id=\"" + " ".join(self.ids) + "\""
+        if len(self.classes) > 0:
+            s += " class=\"" + " ".join(self.classes) + "\""
+        if len(self.attributes) > 0:
+            for p , v in self.attributes:
+                s += " " + p + "=\"" + v + "\""
+        s += ">"
+        for child in self.children:
+            s += child.to_html()
+        s += "</" + self.tag + ">"
+        return s
+
+class TextNode():
+    def __init__(self, text):
+        if len(text) != 0 and text[0] != '\\':
+            text = text.replace("&", "&amp;")
+            text = text.replace("<", "&lt;")
+            text = text.replace(">", "&gt;")
+            text = text.replace("\"", "&quot;")
+        else:
+            text = text[1:]
+        self.text = text
+
+    def br(self):
+        self.text += "<br>"
+        return self
+
+    def to_html(self):
+        return self.text
+
+class LinkNode():
+    def __init__(self, link, text):
+        self.link = link
+        self.text = text
+
+    def to_html(self):
+        s = "<a href=\"" + self.link + "\">"
+        s += self.text
+        s += "</a>"
+        return s
+
+class Br():
+    def to_html(self):
+        return "<br>"
+
+class Hr():
+    def to_html(self):
+        return "<hr>"
+
+########################################
+# Method for making DOM tree
+########################################
+def until_ends_command(name, ptr, lines):
+    i = ptr
+    lst = []
+    while True:
+        if len(lines) <= i:
+            raise Exception("not found ends-command")
+        line = lines[i]
+        if line.startswith(":e-" + name):
+            i += 1
+            break
+        lst.append(line)
+        i += 1
+    return i , lst
+
+def split_and_unescape(s, sep):
+    if s == "":
+        return []
+    i = 0
+    tmp = ""
+    tkn = []
+    if 1 <= len(s) and s[0] == '|':
+        s = s[1:]
+    if 2 <= len(s) and s[-1] == '|' and s[-2] != '\\':
+        s = s[:-1]
+    while True:
+        if len(s) <= i:
+            if tmp != "":
+                tkn.append(tmp)
+            break
+        if s[i] == '\\' and i + 1 < len(s) and \
+           s[i + 1] == sep:
+            tmp += sep
+            i += 2
+        elif s[i] == sep:
+            tkn.append(tmp)
+            tmp = ""
+            i += 1
+        else:
+            tmp += s[i]
+            i += 1
+    return tkn
+
+def define_and_return_domtree(info):
+    # syntax : "(" + id + "," + name + "," + title + ")"
+    id_ , name , title = "" , "" , ""
+    m = re.match(r'\s*\(([^\,]+)\,\s*([^\,]+)\,\s*([^\)]+)\)$', info)
+    if m:
+        id_ = m.group(1)
+        name = m.group(2)
+        title = m.group(3)
+    else:
+        raise Exception("define stc doesn't follow the rule")
+    env[id_] = name
+    span = Element("span.define")
+    b = Element("b")
+    b.appendChild(TextNode(name))
+    span.appendChild(b)
+    span.appendChild(TextNode(" "))
+    span.appendChild(TextNode(title))
+    return span
+
+def make_inf_domtree(info):
+    # syntax : [-name] + "(" conclusion + "," + [ promise1, ... ] ")"
+    name = ""
+    m = re.match(r'-(\S+)\((.*)\)$', info)
+    if m:
+        name = m.group(1)
+        info = m.group(2)
+    else:
+        m = re.match(r'\((.*)\)$', info)
+        if m:
+            name = ""
+            info = m.group(1)
+        else:
+            print info
+            raise Exception("invalid inf: " + info)
+
+    i = 0
+    concl = ""
+    # read until first comma
+    while True:
+        if len(info) <= i:
+            raise Exception("invalid syntax :inf")
+        c = info[i]
+        if c == ',':
+            break
+        elif c == ',' and \
+             i + 1 < len(info) and info[i + 1] == ',':
+            concl += ','
+            i += 2
+        else:
+            concl += c
+            i += 1
+    others = info[i+1:].strip()
+    promises = split_and_unescape(others[1:-1], ',')
+    inf = Element("table.inf")
+    # make domtree for promises
+    up = Element("tr.inf_up")
+    if len(promises) != 0:
+        for p in promises:
+            t = TextNode(p.strip())
+            td = Element("td")
+            td.appendChild(t)
+            up.appendChild(td)
+    else:
+        # add dummy td-element if promises is empty
+        # in order to set name in the middle
+        td = Element("td")
+        td.appendChild(TextNode("\&nbsp;"))
+        up.appendChild(td)
+
+    # add name
+    name_elem = Element("td.inf_name")
+    name_elem.appendChild(TextNode(name))
+    name_elem.setAttribute("rowspan", "2")
+    up.appendChild(name_elem)
+
+    inf.appendChild(up)
+
+    # make domtree for conslusion
+    down = Element("tr.inf_down")
+    dtd = Element("td")
+    dtd.setAttribute("colspan", str(len(promises)))
+    dtd.appendChild(TextNode(concl))
+    down.appendChild(dtd)
+    inf.appendChild(down)
+
+    return inf
+
+def make_item_domtree(lines, with_number):
+    tag = ""
+    if with_number:
+        tag = "ol"
+    else:
+        tag = "ul"
+    item = Element(tag)
+    for line in lines:
+        if len(line) != 0 and line[0] == '-':
+            text = TextNode(line[1:])
+            li = Element("li")
+            li.appendChild(text)
+            item.appendChild(li)
+    return item
+
+def make_horizon_domtree(elems):
+    table = Element("table")
+    tr = Element("tr.horizon")
+    for x in elems:
+        td = Element("td")
+        td.appendChild(x)
+        tr.appendChild(td)
+    table.appendChild(tr)
+    return table
+
+# resolve reference for env
+def scanning_one_line(s):
+    i = 0
+    tmp = ""
+    lst = []
+
+    while i < len(s):
+        c = s[i]
+        if c == ":":
+            if s[i:].startswith(":ref") and i + 4 < len(s):
+                lst.append(TextNode(tmp))
+                tmp = ""
+
+                ss = s[i+4:]
+                i += 4
+
+                # syntax : :ref(id)rest
+                m = m = re.match(r'\(([^\)]+)\).*$', ss)
+                id_ = m.group(1)
+                i += len(id_) + 2
+                if not env.has_key(id_):
+                    raise Exception("not defined " + id_)
+                name = env[id_]
+                link = LinkNode(name, "#" + id_)
+                lst.append(link)
+            else:
+                tmp += c
+                i += 1
+        else:
+            tmp += c
+            i += 1
+    if tmp != "":
+        lst.append(TextNode(tmp))
+
+    if len(lst) == 1:
+        return lst[0]
+    e = Element("span")
+    for x in lst:
+        e.appendChild(x)
+    return e
+
+def make_table_domtree(lines):
+    table = Element("table")
+    for line in lines:
+        tkn2 = split_and_unescape(line, '|')
+
+        # create a single tr node
+        tr = Element("tr")
+        for t in tkn2:
+            td = Element("td")
+            td.appendChild(TextNode(t))
+            tr.appendChild(td)
+        table.appendChild(tr)
+    return table
+
+def make_domtree_at_single_line(ptr, lines):
+    if len(lines) <= ptr:
+        raise Exception("out of lst")
+    line = lines[ptr]
+    if line.startswith(":"):
+        if line.startswith(":inf"):
+            return ptr + 1 , make_inf_domtree(line[4:])
+        elif line.startswith(":define"):
+            return ptr + 1 , define_and_return_domtree(inf[7:])
+        elif line.startswith(":---"):
+            return ptr + 1 , Hr()
+        elif line.startswith(":s-"):
+            m = re.match(r':s-([a-z]+)*', line)
+            if not m:
+                raise Exception("undefined command: " + name)
+            else:
+                name = m.group(1)
+                x , lst = until_ends_command(name, ptr + 1, lines)
+                i = ptr
+                if name == "table":
+                    return x , make_table_domtree(lst)
+                elif name == "itemn":
+                    return x , make_item_domtree(lst, True)
+                elif name == "item":
+                    return x , make_item_domtree(lst, False)
+                elif name == "box":
+                    e = Element("span.box")
+                    a = 0
+                    while a < len(lst):
+                        a2 , b = make_domtree_at_single_line(a, lst)
+                        a = a2
+                        e.appendChild(b)
+                    return x , e
+                elif name == "horizon":
+                    elems = []
+                    a = 0
+                    while a < len(lst):
+                        a2 , b = make_domtree_at_single_line(a, lst)
+                        a = a2
+                        elems.append(b)
+                    return x , make_horizon_domtree(elems)
+                else:
+                    raise Exception("not defined command name " + name)
+    else:
+        #e = scanning_one_line(line)
+        if len(line) == 0 or (len(line) != 0 and line[-1] != '\\'):
+            return ptr + 1 , TextNode(line).br()
+        else:
+            return ptr + 1 , TextNode(line)
+    raise Exception("? " + line)
+
+def make_domtree(lines):
+    root = Element("div")
+    i = 0
+    while True:
+        if len(lines) <= i:
+            break
+        ptr , elem = make_domtree_at_single_line(i, lines)
+        root.appendChild(elem)
+        i = ptr
+    return root
+
+########################################
+# Class for File
+########################################
+class Chapter:
+    def __init__(self, n, title, content, sections):
+        self.n = n
+        self.title = title.rstrip()
+        cnt = []
+        for line in content:
+            cnt.append(line.rstrip())
+        self.content = cnt
+        self.sections = sorted(sections, key=lambda x: x.n)
+
+    def to_html(self):
+        idname = "ch" + str(self.n)
+        elem = Element("div.ch#" + idname)
+        link = LinkNode("#" + idname, "&nbsp;#&nbsp;")
+        elem.appendChild(link)
+        title = TextNode(" chapter " + str(self.n) + ". " + self.title)
+        elem.appendChild(Element("span.ch_title").appendChild(title))
+        if self.content != []:
+            elem.appendChild(make_domtree(self.content).addClass("ch_content"))
+
+        ul = Element("ul.no_num")
+        for sec in self.sections:
+            li = Element("li")
+            li.appendChild(sec.to_html())
+            ul.appendChild(li)
+        elem.appendChild(ul)
+        return elem
+
+    def damp(self):
+        print "chapter-start " + str(self.n)
+        print self.title
+        for line in self.content:
+            print line
+        for sec in self.sections:
+            sec.damp()
+        print "chapter-start " + str(self.n)
+
+class Section:
+    def __init__(self, ch_n, n, title, content):
+        self.ch_n = ch_n
+        self.n = n
+        self.title = title.rstrip()
+        cnt = []
+        for line in content:
+            cnt.append(line.rstrip())
+        self.content = cnt
+
+    def to_html(self):
+        idname = "sec" + str(self.ch_n) + "_" + str(self.n)
+        elem = Element("div.sec#" + idname)
+        link = LinkNode("#" + idname, "&nbsp;#&nbsp;")
+        elem.appendChild(link)
+        title = TextNode(" section " + str(self.ch_n) + \
+                         "-" + str(self.n) + ". " + self.title)
+        elem.appendChild(Element("span.sec_title").appendChild(title))
+        elem.appendChild(make_domtree(self.content).addClass("sec_content"))
+        return elem
+
+    def damp(self):
+        print "section-start " + str(self.n)
+        print self.title
+        for line in self.content:
+            print line
+        print "section-end " + str(self.n)
+
+########################################
+# Class for Style sheet
+########################################
+class Styles():
+    def __init__(self):
+        self.styles = []
+
+    def appendBlock(self, block):
+        self.styles.append(block)
+
+    def to_css(self):
+        s = ""
+        for style in self.styles:
+            s += style.to_css()
+            s += "\n"
+        return s
+    
+class StyleBlock():
+    def __init__(self, secretor):
+        self.secretor = secretor
+        self.props = []
+
+    def appendProp(self, name, value):
+        self.props.append((name, value))
+        return self.props
+
+    def to_css(self):
+        s = self.secretor
+        s += " {\n"
+        for n , v in self.props:
+            s += n + ": " + v + ";\n"
+        s += "}"
+        return s
+        
+        
