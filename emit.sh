73d1bff1e098dea6971867221abac63560c08805
Haruka Matsumoto <harukam0416@gmail.com>
2021-10-05 17:38:41 +0900

    Add index html

--- emit.sh
+++ emit.sh
@@ -4,10 +4,8 @@ src_dir="src"
 test_dir="test"
 out_dir="docs"
 
-if [ ! -d $out_dir ]
-then
-    mkdir $out_dir
-fi
+rm -rf $out_dir
+mkdir -p $out_dir
 
 for fname in `ls ${test_dir}/*.txt | xargs -n 1 basename`
 do
@@ -20,3 +18,27 @@ do
     exit 1
   fi
 done
+
+files="$(ls $out_dir | sort -n)"
+index_file="$out_dir/index.html"
+
+echo """<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta NAME="viewport" content='width=device-width, initial-scale=1' />
+    <title>Zemidown docs</title>
+    <style>
+    </style>
+  </head>
+  <body>
+    <h1>Files</h1>
+    <ul>""" >> "$index_file"
+
+for file in $files; do
+  echo "<li><a href='$file'>$file</a></li>" >> "$index_file"
+done
+
+echo """
+  </ul></body>
+</html>""" >> "$index_file"
